7ff1b31533f4148b5331a13a4579aed7a7a2230c
Shiyu Wang <shiyu3169@gmail.com>
2019-09-03 14:01:00 -0400

    start front end

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// Components
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className='container'>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
