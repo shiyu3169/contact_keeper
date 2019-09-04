ba2a83cb0ec4a15642150ab5a6e874c660f56bd1
Shiyu Wang <shiyu3169@gmail.com>
2019-09-04 09:15:35 -0400

    temp

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -42,8 +42,15 @@ const ContactState = props => {
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add Contact
+  const addContact = contact => {
+    contact.id = uuid.v4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   // Delete Contact
+  const deleteContact = id => {
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   // Set Current Contact
 
@@ -58,7 +65,8 @@ const ContactState = props => {
   return (
     <ContactContext.Provider
       value={{
-        contacts: state.contacts
+        contacts: state.contacts,
+        addContact
       }}
     >
       {props.children}
