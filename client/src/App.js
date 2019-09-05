f89d5add6992956e587488f3a7ceb22c55d9e095
Shiyu Wang <shiyu3169@gmail.com>
2019-09-05 10:18:15 -0400

    login register

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import ContactState from './context/contact/ContactState';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// Context
+import AuthState from "./context/auth/AuthState";
+import ContactState from "./context/contact/ContactState";
 // Components
-import Navbar from './components/layout/Navbar';
-import Home from './components/pages/Home';
-import About from './components/pages/About';
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 function App() {
   return (
-    <ContactState>
-      <Router>
-        <Navbar />
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-        </div>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </div>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ title, icon }) => {
   return (
-    <div className='navbar bg-primary'>
+    <div className="navbar bg-primary">
       <h1>
         <i className={icon} /> {title}
       </h1>
       <ul>
         <li>
-          <Link to='/'>Home</Link>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to='/about'>About</Link>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
         </li>
       </ul>
     </div>
@@ -26,8 +32,8 @@ Navbar.prototypes = {
 };
 
 Navbar.defaultProps = {
-  title: 'Contact Keeper',
-  icon: 'fas fa-id-card-alt'
+  title: "Contact Keeper",
+  icon: "fas fa-id-card-alt"
 };
 
 export default Navbar;
